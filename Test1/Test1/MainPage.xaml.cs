da5e3aa754b127ac201cd865b6aea6794fc5172a
unknown <sashaivanov161192@gmail.com>
2018-10-25 13:54:12 +0300

    commit

---
+++ Test1/Test1/MainPage.xaml.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+
+namespace Test1
+{
+
+    public partial class MainPage : ContentPage
+    {
+
+        public MainPage()
+        {
+            InitializeComponent();
+         
+        }
+
+        private void OnButtonClicked(object sender, System.EventArgs e) {
+            
+            if (!String.IsNullOrEmpty(myEnasd.Text)&&!String.IsNullOrEmpty(entryPassword.Text)){
+                if (myEnasd.Text.Length > 6 && EmailValidate(myEnasd.Text))
+                {
+                    if (entryPassword.Text.Length > 6)
+                    {
+                        ToSecondForm();
+           
+                    }
+                    else
+                    {
+                        Device.BeginInvokeOnMainThread(() =>
+                        {
+                            DisplayAlert("Password Error", "Incorrect Password", "OK");
+                        });
+                    }
+
+                }
+                else
+                {
+                    Device.BeginInvokeOnMainThread(() =>
+                    {
+                        DisplayAlert("Email Error", "Incorrect Email", "OK");
+                    });
+                }
+            }
+
+
+
+        }
+
+        private bool EmailValidate(string s) {
+          
+            string a = @"^([\w\.\-]+)@([\w\-]+)((\.(\w){2,3})+)$";
+           
+            Regex regex = new Regex(a);
+            Match match = regex.Match(s);
+
+            
+
+            return match.Success;
+        }
+
+        private async void ToSecondForm()
+        {
+            await Navigation.PushModalAsync(new SecondForm());
+        }
+    }
+}
---
+++ Test1/Test1/SecondForm.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace Test1
+{
+	public class SecondForm : ContentPage
+	{
+		public SecondForm ()
+		{
+			Content = new StackLayout {
+				Children = {
+					new Label { Text = "Welcome to Xamarin.Forms!" }
+				}
+			};
+		}
+	}
+}
